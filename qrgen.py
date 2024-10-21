58add747d0bc98936de86ddf8be67a318a49b669
Ghafoor <ghafoornazari@proton.me>
2024-10-21 18:31:14 +0200

    Update qrgen.py
    Added the option for the user to choose the color for the QR code and also for the background.

--- qrgen.py
+++ qrgen.py
@@ -5,11 +5,13 @@ import qrcode.constants
 source = input("Enter the source link: ")
 file_name = input("Save file as: (Enter a name without the format)")
 file_name = file_name + ".png"
+qr_color = input("What color do you want the QR code? ")
+bg_color = input("What color do you want the background? ")
 
 qr = qrcode.QRCode(version=1,
                    error_correction=qrcode.constants.ERROR_CORRECT_H,
                    box_size=10, border=10,)
 qr.add_data(source)
 qr.make(fit=True)
-img=qr.make_image(fill_color="red", back_color="black")
+img=qr.make_image(fill_color=qr_color, back_color=bg_color)
 img.save(file_name)
